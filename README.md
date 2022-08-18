6ac83f5863d038a785b1c47122ec1b51d96caaf0
alexander k <alexander.koronovskiy@gmail.com>
2022-08-18 17:25:59 +0400

    view proj init

---
+++ README.md
@@ -0,0 +1,24 @@
+# assessing-floor-client
+
+# Запуск клиента
+```
+npm run serve
+```
+
+# Запуск тестового сервера
+
+## Инициализация сервера из консоли:
+
+```bash
+cd yksinner/assessing-floor && 
+python3 -m venv venv && 
+source venv/bin/activate &&
+pip install -r requirements.txt &&
+python3 main.py
+```
+
+## Перезапуск сервера из корневой папки:
+
+```bash
+python3 main.py
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<!-- Use preprocessors via the lang attribute! e.g. <template lang="pug"> -->
+<template>
+  <div id="app">
+    <div v-if="data" class="container">
+      <img
+        :src="data.plan_image.image_thumb"
+        :width="data.plan_image.width"
+        :height="data.plan_image.height"
+        class="container__image"
+      />
+      <div class="container__hint">
+        <svg :viewBox="`0 0 ${data.plan_image.width} ${data.plan_image.height}`" xmlns="http://www.w3.org/2000/svg">
+          <template :key="flat.id" v-for="flat in data.flats">
+            <polygon :points="flat.area_points" fill="red" />
+          </template>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: null
+    };
+  },
+  methods: {
+    doSomething() {
+      alert('Hello!');
+    }
+  },
+  mounted () {
+    fetch('https://api.gkosnova.tech/public/api/v1/building-objects/2/floor/1/1/-1?fullList=1')
+      .then(res => res.json())
+      .then(({ data }) => {
+        this.data = data
+      })
+
+  }
+};
+</script>
+
+<!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
+<style>
+  .container {
+    position: relative;
+  }
+  .container__hint {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .container__image {
+    width: 100%;
+    object-fit: contain;
+    height: auto;
+  }
+</style>
