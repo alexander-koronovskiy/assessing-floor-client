8517d133c93a388f5fc42c57b336459b22732a3b
alexander k <alexander.koronovskiy@gmail.com>
2022-08-19 17:41:02 +0400

    interim 02

--- src/components/Connection.vue
+++ src/components/Connection.vue
@@ -1,55 +1,43 @@
 <template>
 <div id="app">
   <input type="file" @change="onFileChange" />
-  <div class="container">
-    <img 
-      v-if="url"
-      :src="data.plan_image.image_thumb"
-      :width="data.plan_image.width"
-      :height="data.plan_image.height"
-      class="container__image"
-    />
+
+  <div id="preview">
+    <img v-if="url" :src="url" />
   </div>
+
 </div>
 </template>
 
 <script>
 export default {
-data() {
-  return {
-    url: null,
-  }
-},
-methods: {
-  onFileChange(e) {
+  data() {
+    return {
+      url: null,
+    }
+  },
+  methods: {
+    onFileChange(e) {
       const file = e.target.files[0];
       this.url = URL.createObjectURL(file);
+      let Data = new FormData();
+      Data.append('image', file);
+
+      fetch('http://127.0.0.1:8000/image', {
+        method: "POST",
+        body: Data
+      }).then(response => {
+        if (response.status !== 200) {
+          return Promise.reject();
+        }
+        return response.json();
+      }).then(function(data) {
+        alert(JSON.stringify(data))
+      }).catch(() => alert('ошибка'));
+    }
   }
-},
-mounted () {
-  fetch('https://api.gkosnova.tech/public/api/v1/building-objects/2/floor/1/1/-1?fullList=1')
-  .then(res => res.json())
-  .then(({ data }) => {
-    this.data = data
-  })
-}
 };
 </script>
 
 <style>
-  .container {
-    position: relative;
-  }
-  .container__hint {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-  .container__image {
-    width: 100%;
-    object-fit: contain;
-    height: auto;
-  }
 </style>
