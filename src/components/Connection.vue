5580634d13b42ddacdcae8840f25b6effb705acb
alexander k <alexander.koronovskiy@gmail.com>
2022-08-26 13:10:33 +0400

    v.0.2

--- src/components/Connection.vue
+++ src/components/Connection.vue
@@ -1,13 +1,16 @@
 <template>
 <div class="container">
-  <input type="file" @change="onFileChange" />
-  <img v-if="url" :src="url" :width="imageWidth" :height="imageHeight" class="container__image"/>
-  <div v-show="flats.length" class="container__hint">
-    <svg :viewBox="`0 0 ${imageWidth} ${imageHeight}`" xmlns="http://www.w3.org/2000/svg">
-      <template :key="id" v-for="(flat, id) in flats">
-        <polygon :points="flat" fill="green" fill-opacity="0.4" />
-      </template>
-    </svg>
+  <div v-show="loading" class="donut"></div>
+  <input type="file" v-show="!loading" @change="onFileChange" />
+  <div class="container__image">
+    <img v-if="url" :src="url" :width="imageWidth" :height="imageHeight" class="container__image--img"/>
+    <div v-show="flats.length" class="container__image--hint">
+      <svg :viewBox="`0 0 ${imageWidth} ${imageHeight}`" xmlns="http://www.w3.org/2000/svg">
+        <template :key="id" v-for="(flat, id) in flats">
+          <polygon :points="flat" fill="green" fill-opacity="0.4" />
+        </template>
+      </svg>
+    </div>
   </div>
 </div>
 </template>
@@ -20,6 +23,7 @@ export default {
       imageWidth: 0,
       imageHeight: 0,
       flats: [],
+      loading: false,
     }
   },
   methods: {
@@ -32,7 +36,7 @@ export default {
         this.imageWidth = prefetchImage.width;
         this.imageHeight = prefetchImage.height;
       }
-      
+      this.loading = true;
       let Data = new FormData();
       Data.append('image', file);
       fetch('http://127.0.0.1:8000/image', {
@@ -47,27 +51,47 @@ export default {
         this.flats = data.data.map(flat => {
           return flat.join(' ');
         });
-      })
+        this.loading = false;
+      });
     }
   }
 };
 </script>
 
 <style>
-.container {
+.container__image {
   position: relative;
 }
-.container__hint {
+.container__image--hint {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
 }
-.container__image {
+.container__image--img {
   width: 100%;
   object-fit: contain;
   height: auto;
-  margin-top: -1.5%;
+}
+
+@keyframes donut-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.donut {
+  display: inline-block;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #52e235;
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+  animation: donut-spin 1.2s linear infinite;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
